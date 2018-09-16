01ad0a487dfea72d281af1ade2b666cb84e1734c
Andrew <andy.birks@gmail.com>
2018-09-16 09:42:15 +0100

    Video 11 - Async / Await - Error Catching

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
 
 exports.homePage = (req,res) => {
     res.render('index');
@@ -10,8 +12,9 @@ exports.addStore = (req,res) => {
     });
 }
 
-exports.createStore = (req,res) => {
-    //console.log( req.body);
-
-    res.json(req.body);
+exports.createStore = async (req,res) => {
+   const store = new Store(req.body)
+   await store.save()
+   console.log("It worked");
+   res.redirect('/');
 }
