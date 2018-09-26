9e5896206fd4c0e122cabcef90eb6b1ea32b5992
Andrew <andrew@purplecs.com>
2018-09-26 17:18:43 +0100

    Video 27 - Reset Password flow - About 16mins in - About to Update password

--- dang-thats-delicious/controllers/authController.js
+++ dang-thats-delicious/controllers/authController.js
@@ -1,4 +1,7 @@
 const passport = require("passport");
+const crypto = require("crypto");
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
 
 exports.login = passport.authenticate("local", {
   failureRedirect: "/login",
@@ -23,4 +26,47 @@ exports.isLoggedIn = (req, res, next) => {
 
   req.flash("error", "You must be logged in to do that!");
   res.redirect("/login");
+};
+
+exports.forgot = async (req, res) => {
+  // 1. See if user exists with email
+  const user = await User.findOne({
+    email: req.body.email
+  });
+
+  if (!user) {
+    req.flash("error", "No account with that email exists.");
+    return res.redirect("/login");
+  }
+  // 2. Set reset token and expiry on their account
+  user.resetPasswordToken = crypto.randomBytes(20).toString("hex");
+  user.resetPasswordExpires = Date.now() + 3600000;
+  await user.save();
+  // 3. Send them an email with the token
+  const resetURL = `http://${req.headers.host}/account/reset/${
+    user.resetPasswordToken
+  }`;
+  req.flash(
+    "success",
+    `You have been emailed a password reset link. ${resetURL}`
+  );
+  // 4. Redirect to login page
+  res.redirect("/login");
+};
+
+exports.reset = async (req, res) => {
+  //Take the token and check if someone has that token
+  //And that the Expiry date, which was set to last for an hour - Is still ahead of "the now".
+  const user = await User.findOne({
+    resetPasswordToken: req.params.token,
+    resetPasswordExpires: { $gt: Date.now() } // Check the date is still in the future - Greater than the current date
+  });
+
+  if (!user) {
+    req.flash("error", "Password reset token is invalid, or has expired");
+    return res.redirect("/login");
+  }
+
+  //If there is a user, show the password reset form
+  res.render("reset", { title: "Reset your password" });
 };
--- dang-thats-delicious/models/User.js
+++ dang-thats-delicious/models/User.js
@@ -20,7 +20,9 @@ const userSchema = new Schema({
     type: String,
     required: "Please Supply a Name",
     trim: true //Clean whitespace
-  }
+  },
+  resetPasswordToken: String,
+  resetPasswordExpires: Date
 });
 
 userSchema.virtual("gravatar").get(function() {
