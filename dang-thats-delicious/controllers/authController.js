5df321db23429b7a5c1ddd1719cc1735f2c7b981
Andrew <andrew@purplecs.com>
2018-09-26 11:00:27 +0100

    Check if user is logged in before allowing them to add a store. - End of video 25

--- dang-thats-delicious/controllers/authController.js
+++ dang-thats-delicious/controllers/authController.js
@@ -1,14 +1,26 @@
-const passport = require('passport');
+const passport = require("passport");
 
-exports.login = passport.authenticate('local', {
-    failureRedirect: '/login',
-    failureFlash: 'Failed Login!',
-    successRedirect: '/',
-    successFlash: 'You now are logged in!'
-})
+exports.login = passport.authenticate("local", {
+  failureRedirect: "/login",
+  failureFlash: "Failed Login!",
+  successRedirect: "/",
+  successFlash: "You now are logged in!"
+});
 
-exports.logout = (req,res)=>{
-    req.logout();
-    req.flash('success', 'You are now logged out')
-    res.redirect('/');
-}
+exports.logout = (req, res) => {
+  req.logout();
+  req.flash("success", "You are now logged out");
+  res.redirect("/");
+};
+
+exports.isLoggedIn = (req, res, next) => {
+  //First check if the user is authenticated
+
+  if (req.isAuthenticated()) {
+    next(); // Carry on! They are logged in
+    return;
+  }
+
+  req.flash("error", "You must be logged in to do that!");
+  res.redirect("/login");
+};
