ae28fb0ce6607e6c489dfec6f3d8da3dcd468186
Andrew <andrew@purplecs.com>
2018-09-26 10:45:24 +0100

    Logout and Login - Baked into Passport.js - Lesson 25 - 3min in

--- dang-thats-delicious/controllers/authController.js
+++ dang-thats-delicious/controllers/authController.js
@@ -6,3 +6,9 @@ exports.login = passport.authenticate('local', {
     successRedirect: '/',
     successFlash: 'You now are logged in!'
 })
+
+exports.logout = (req,res)=>{
+    req.logout();
+    req.flash('success', 'You are now logged out')
+    res.redirect('/');
+}
--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -28,6 +28,7 @@ router.get("/tags", catchErrors(storeController.getStoresByTag));
 router.get("/tags/:tag", catchErrors(storeController.getStoresByTag));
 
 router.get("/login", userController.loginForm);
+router.post("/login", authController.login);
 router.get("/register", userController.registerForm);
 
 //1. Validate the registration data
@@ -39,5 +40,11 @@ router.post(
   userController.register,
   authController.login
 );
+
+router.get(
+  "/logout",
+  authController.logout
+);
+
 
 module.exports = router;
