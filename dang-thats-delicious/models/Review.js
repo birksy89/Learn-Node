38d068344bb1ef8c85ece6d41ba8436172667c4b
Andrew <andrew@purplecs.com>
2018-10-11 16:48:00 +0100

    Starting Lesson 37 - Creating Review Model

---
+++ dang-thats-delicious/models/Review.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+
+const reviewSchema = new mongoose.Schema({
+
+})
+
+
+module.exports = mongoose.model("Review", reviewSchema);
