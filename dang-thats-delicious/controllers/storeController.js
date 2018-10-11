e2ba7fa30f7dda0f2c4f74dda7d9b52b30cd00a8
Andrew <andrew@purplecs.com>
2018-10-11 15:27:16 +0100

    Lesson 35 - 14min in - Creating "Hearting" functionality using Form/Post -Not AJAX yet

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Store = mongoose.model("Store");
+const User = mongoose.model("User");
 const multer = require("multer");
 const jimp = require("jimp");
 const uuid = require("uuid");
@@ -204,12 +205,22 @@ exports.mapStores = async (req, res) => {
   res.json(stores);
 };
 
-
-
 exports.mapPage = async (req, res) => {
-
-  res.render('map', {
-    title:'Map'
+  res.render("map", {
+    title: "Map"
   });
 };
 
+exports.heartStore = async (req, res) => {
+  const hearts = req.user.hearts.map(obj => {
+    return obj.toString();
+  });
+
+  //Will the heart be added or removed?
+  const operator = hearts.includes(req.params.id) ? "$pull" : "$addToSet";
+
+  const user = await User.findByIdAndUpdate(req.user._id, {
+    [operator]: { hearts: req.params.id }
+  },{new: true});
+  res.json(user);
+};
