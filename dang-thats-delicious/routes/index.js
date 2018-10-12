98619886bf79df1dee391bf1ad84b230be533463
Andrew <andrew@purplecs.com>
2018-10-12 13:45:23 +0100

    Posting Reviews to new Controller - End of Video 37

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const storeController = require("../controllers/storeController");
 const userController = require("../controllers/userController");
 const authController = require("../controllers/authController");
+const reviewController = require("../controllers/reviewController");
 const { catchErrors } = require("../handlers/errorHandlers");
 
 // Do work here
@@ -54,7 +55,18 @@ router.post(
 );
 
 router.get("/map", catchErrors(storeController.mapPage));
-router.get("/hearts", authController.isLoggedIn, catchErrors(storeController.getHearts));
+
+router.get(
+  "/hearts",
+  authController.isLoggedIn,
+  catchErrors(storeController.getHearts)
+);
+
+router.post(
+  "/reviews/:id",
+  authController.isLoggedIn,
+  catchErrors(reviewController.addReview)
+);
 
 /*
   API
@@ -65,6 +77,5 @@ router.get("/api/search", catchErrors(storeController.searchStores));
 router.get("/api/stores/near", catchErrors(storeController.mapStores));
 
 router.post("/api/stores/:id/heart", catchErrors(storeController.heartStore));
-
 
 module.exports = router;
